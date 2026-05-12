8aa9ed9567c9f5a07168f8b63b8727d24b38e8ba
Philip Fabunan <Philly@Ghost.home.local>
2026-05-12 14:05:41 -0700

    Update index.js with Hello World server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// write your code here
+// import http module and create web server
+const http = require('http');
+
+// create server
+const server = http.createServer((req, res) => {
+
+  // log request path
+  console.log(req.url);
+
+  // status code
+  res.statusCode = 200;
+
+  // headers
+  res.setHeader('Content-Type', 'text/plain');
+
+  // response body
+  res.end('Hello World');
+});
+
+// need to start andlisten on port 8080 for requests
+server.listen(8080, () => {
+
+  console.log('Server listening on port 8080');
+});
